f4427de81e84a66d9a91d2dc70d9770af4bda27f
manthofani <manthofani4u@gmail.com>
2025-09-20 01:41:11 +0700

    coba minimal

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const app = require("./server");
+const express = require("express");
+const app = express();
+
+app.get("/api/hello", (req, res) => {
+  res.json({ msg: "Minimal Express works!" });
+});
+
 module.exports = app;
